f59b40cb8bc98a2068c506ac14e6a363458fd7f1
ros3003 <rkudybyn@gmail.com>
2021-06-01 12:06:12 +0300

    test

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -14,6 +14,7 @@ const PostAddForm = () => {
                 type="submit"
                 className="btn btn-outline-secondary">
                 Добавить </button>
+                <button>test</button>
         </form>
     )
 }
